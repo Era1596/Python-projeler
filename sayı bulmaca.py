e56988a9da879cf14924fcda5083f554e38ac5fd
Era1596 <93435277+Era1596@users.noreply.github.com>
2022-01-08 18:46:35 +0300

    Update sayı bulmaca.py

--- sayı bulmaca.py
+++ sayı bulmaca.py
@@ -1,7 +1,7 @@
 import random
 while True:
-    deger=random.randint(1,9)
-    deger2=int(input("1 ile 10 arasında tuttuğum sayıyı tahmin et:"))
+    deger=random.randint(1,5)
+    deger2=int(input("1 ile 5 arasında tuttuğum sayıyı tahmin et:"))
     if deger==deger2:
         deger3=int(input("Doğru, programı yeniden başlatmak için 1 e bitirmek için 2 ye bas"))
         if deger3==1:
@@ -12,10 +12,17 @@ while True:
             break
     else:
         print("Cevap",deger)
-        deger4=int(input("Yanlış, programı yeniden başlatmak için 1 e bitirmek için 2 ye bas"))
-        if deger4==1:
-            print("Program yeniden başlatıldı")
+        try:
+            deger4=int(input("Yanlış, programı yeniden başlatmak için 1 e bitirmek için 2 ye bas"))
+            if deger4==1:
+                print("Program yeniden başlatıldı")
+                continue
+            elif deger4==2:
+                print("Program bitirildi")
+                break
+            else:
+                print("1 e veya 2 ye basmalısın,Program yeeniden başlatılıyor")
+                continue
+        except ValueError:
+            print("1 e veya 2 ye basmalısın, Program yeniden başlatılıyor")
             continue
-        elif deger4==2:
-            print("Program bitirildi")
-            break
